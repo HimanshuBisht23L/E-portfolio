335483f6b72c2e849fd4aa122c32f94503b763fb
HimanshuBisht23L <himanshiubisht4532@gmail.com>
2025-04-29 23:00:29 +0530

    video added

--- src/components/Intropage.jsx
+++ src/components/Intropage.jsx
@@ -4,7 +4,7 @@ import "../styles/intropage.css"
 export default function Intropage() {
   return (
     <div className='intro-vdo'>
-        <iframe src="https://drive.google.com/file/d/1GPuoyvTrbSpiZH8yKy29LXW0CnHusxNC/preview" width="640" height="480" allow="autoplay" allowFullScreen></iframe>
+        <iframe src="https://drive.google.com/file/d/1Po1boM_Wlhd6C0Q1zFhF6cSBtH04VPZr/view?usp=drive_link" width="640" height="480" allow="autoplay" allowFullScreen></iframe>
     </div>
   )
 }
